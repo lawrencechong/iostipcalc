033a41fd734b1f37d7eaf3b4cccda933c1f469af
Lawrence Chong <mail.to.lawrence@gmail.com>
2016-01-01 14:32:32 -0500

    fixed a bug with custom tip and added split tip feature

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var customTipField: UITextField!
+    @IBOutlet weak var groupSizeField: UILabel!
+    @IBOutlet weak var splitPayField: UILabel!
+    @IBOutlet weak var groupSizeStepper: UIStepper!
     
     
     override func viewDidLoad() {
@@ -23,6 +26,8 @@ class ViewController: UIViewController {
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
         customTipField.text = "--"
+        groupSizeField.text = "1"
+        splitPayField.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,18 +37,27 @@ class ViewController: UIViewController {
 
     @IBAction func onEditingChange(sender: AnyObject) {
         
-        var tipPercent = [0.18, 0.2, 0.22]
-        var tipAmt = tipPercent[tipControl.selectedSegmentIndex]
+        var tipAmt = 0.0
+        
+        if ( tipControl.selectedSegmentIndex != -1){
+            var tipPercent = [0.18, 0.2, 0.22]
+            tipAmt = tipPercent[tipControl.selectedSegmentIndex]}
+        else{
+            var customtip = NSString(string: customTipField.text!).doubleValue
+            tipAmt = (customtip/100)
+        }
         
         var billAmount = NSString(string: billField.text!).doubleValue
         var tip = billAmount * tipAmt
         var total = billAmount + tip
         
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(total)"
-        
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        splitPayField.text = String(format: "$%.2f", total)
+        
+        var people = groupSizeStepper.value
+        var costPerPerson = total/people
+        splitPayField.text = String(format: "$%.2f", costPerPerson)
     }
     
     @IBAction func onSegmentedTIpPress(sender: AnyObject) {
@@ -62,15 +76,40 @@ class ViewController: UIViewController {
         var tip = billAmount * (customtip/100)
         var total = billAmount + tip
         
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(total)"
-        
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
+        var people = groupSizeStepper.value
+        var costPerPerson = total/people
+        splitPayField.text = String(format: "$%.2f", costPerPerson)
     }
+    
+    @IBAction func onStepperChange(sender: AnyObject) {
+        
+        groupSizeField.text = String(stringInterpolationSegment: Int(groupSizeStepper.value))
+        
+        var people = groupSizeStepper.value
 
-    @IBAction func onCustomTipTouch(sender: AnyObject) {
-        customTipField.text = ""
+        var tipAmt = 0.0
+        
+        if ( tipControl.selectedSegmentIndex != -1){
+            var tipPercent = [0.18, 0.2, 0.22]
+            tipAmt = tipPercent[tipControl.selectedSegmentIndex]}
+        else{
+            var customtip = NSString(string: customTipField.text!).doubleValue
+            tipAmt = (customtip/100)
+        }
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * tipAmt
+        var total = billAmount + tip
+        
+        var costPerPerson = total/people
+
+        splitPayField.text = String(format: "$%.2f", costPerPerson)
+        
     }
+    
+
 }
 
